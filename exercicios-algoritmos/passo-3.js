5d8db5d7013bf1f3721d849fac42458fe1b8a6d7
CharlieBellow <Charliebftm@gmail.com>
2022-05-10 16:40:39 -0300

    adicionando mais exercicios

---
+++ exercicios-algoritmos/passo-3.js
@@ -0,0 +1,101 @@
+// Passo 3 - CONDIÇÕES BÁSICAS - Lista de Exercícios Guanabara
+
+/*26) Escreva um algoritmo que leia dois números inteiros e compare-os, mostrando
+na tela uma das mensagens abaixo:
+- O primeiro valor é o maior
+- O segundo valor é o maior
+  - Não existe valor maior, os dois são iguais*/
+
+
+/*27) Crie um programa que leia duas notas de um aluno e calcule a sua média,
+mostrando uma mensagem no final, de acordo com a média atingida:
+- Média até 4.9: REPROVADO
+- Média entre 5.0 e 6.9: RECUPERAÇÃO
+  - Média 7.0 ou superior: APROVADO*/
+
+
+/*28) Faça um programa que leia a largura e o comprimento de um terreno
+retangular, calculando e mostrando a sua área em m². O programa também
+devemostrar a classificação desse terreno, de acordo com a lista abaixo:
+- Abaixo de 100m² = TERRENO POPULAR
+- Entre 100m² e 500m² = TERRENO MASTER
+  - Acima de 500m² = TERRENO VIP*/
+
+  
+/*29) Desenvolva um programa que leia o nome de um funcionário, seu salário,
+quantos anos ele trabalha na empresa e mostre seu novo salário, reajustado de
+acordo com a tabela a seguir:
+- Até 3 anos de empresa: aumento de 3%
+- entre 3 e 10 anos: aumento de 12.5%
+- 10 anos ou mais: aumento de 20%*/
+
+  
+/*30) [DESAFIO] Refaça o algoritmo 25, acrescentando o recurso de mostrar que tipo
+de triângulo será formado:
+- EQUILÁTERO: todos os lados iguais
+- ISÓSCELES: dois lados iguais
+  - ESCALENO: todos os lados diferentes*/
+
+
+/*31) [DESAFIO] Crie um jogo de JoKenPo (Pedra-Papel-Tesoura)
+*/
+
+/*32) [DESAFIO] Crie um jogo onde o computador vai sortear um número entre 1 e 5 o
+jogador vai tentar descobrir qual foi o valor sorteado.
+*/
+
+/*33) Escreva um programa para aprovar ou não o empréstimo bancário para a compra
+de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e
+em quantos anos ele vai pagar. Calcule o valor da prestação mensal, sabendo que
+ela não pode exceder 30 % do salário ou então o empréstimo será negado.*/
+
+
+/*34) O Índice de Massa Corpórea (IMC) é um valor calculado baseado na altura e no
+peso de uma pessoa. De acordo com o valor do IMC, podemos classificar o
+indivíduo dentro de certas faixas.
+- abaixo de 18.5: Abaixo do peso
+- entre 18.5 e 25: Peso ideal
+- entre 25 e 30: Sobrepeso
+- entre 30 e 40: Obesidade
+- acima de 40: Obseidade mórbida
+Obs: O IMC é calculado pela expressão peso/altura² (peso dividido pelo quadrado
+da altura)*/
+
+
+/*35) Uma empresa de aluguel de carros precisa cobrar pelos seus serviços. O
+aluguel de um carro custa R$90 por dia para carro popular e R$150 por dia para
+carro de luxo. Além disso, o cliente paga por Km percorrido. Faça um programa
+que leia o tipo de carro alugado (popular ou luxo), quantos dias de aluguel e
+quantos Km foram percorridos. No final mostre o preço a ser pago de acordo com a
+tabela a seguir:
+- Carros populares (aluguel de R$90 por dia)
+- Até 100Km percorridos: R$0,20 por Km
+- Acima de 100Km percorridos: R$0,10 por Km
+- Carros de luxo (aluguel de R$150 por dia)
+- Até 200Km percorridos: R$0,30 por Km
+  - Acima de 200Km percorridos: R$0, 25 por Km*/
+
+
+/*36) Um programa de vida saudável quer dar pontos atividades físicas que podem
+ser trocados por dinheiro. O sistema funciona assim:
+- Cada hora de atividade física no mês vale pontos
+- até 10h de atividade no mês: ganha 2 pontos por hora
+- de 10h até 20h de atividade no mês: ganha 5 pontos por hora
+- acima de 20h de atividade no mês: ganha 10 pontos por hora
+- A cada ponto ganho, o cliente fatura R$0,05 (5 centavos)
+Faça um programa que leia quantas horas de atividade uma pessoa teve por mês,
+  calcule e mostre quantos pontos ela teve e quanto dinheiro ela conseguiu ganhar.*/
+
+
+/*37) Uma empresa precisa reajustar o salário dos seus funcionários, dando um
+aumento de acordo com alguns fatores. Faça um programa que leia o salário atual,
+o gênero do funcionário e há quantos anos esse funcionário trabalha na empresa.
+No final, mostre o seu novo salário, baseado na tabela a seguir:
+- Mulheres
+- menos de 15 anos de empresa: +5%
+- de 15 até 20 anos de empresa: +12%
+- mais de 20 anos de empresa: +23%
+- Homens
+- menos de 20 anos de empresa: +3%
+- de 20 até 30 anos de empresa: +13%
+- mais de 30 anos de empresa: +25%*/
