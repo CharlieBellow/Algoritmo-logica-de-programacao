fadcdc998dac61d726a54b0a6d48c96f41eb7c72
CharlieBellow <Charliebftm@gmail.com>
2022-06-25 15:10:37 -0300

    adicionando passos 6 e 7

---
+++ Lista-exercicios-algoritmos/passo-6.js
@@ -0,0 +1,22 @@
+//PASSO 06 – REPETIÇÃO COM FAÇA ENQUANTO
+
+/*61) Crie um programa que mostre na tela a seguinte contagem, usando a estrutura
+“faça enquanto”
+0 3 6 9 12 15 18 21 24 27 30 Acabou!*/
+
+/*62) Faça um programa usando a estrutura “faça enquanto” que leia a idade de
+várias pessoas. A cada laço, você deverá perguntar para o usuário se ele quer ou
+não continuar a digitar dados. No final, quando o usuário decidir parar, mostre
+na tela:
+a) Quantas idades foram digitadas
+b) Qual é a média entre as idades digitadas
+c) Quantas pessoas tem 21 anos ou mais.*/
+
+
+/*63) Crie um programa usando a estrutura “faça enquanto” que leia vários números.
+A cada laço, pergunte se o usuário quer continuar ou não. No final, mostre na
+tela:
+a) O somatório entre todos os valores
+b) Qual foi o menor valor digitado
+c) A média entre todos os valores
+d) Quantos valores são pares*/
